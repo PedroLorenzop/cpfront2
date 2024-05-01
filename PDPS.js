73990d748d4559d6988f8b5013b7b5c53b6ae879
Valieris <148988047+Valieris@users.noreply.github.com>
2024-04-30 21:52:50 -0300

    PDP e POP-UP na PDP
    PDP dos 3 produtos com o POP- UP animado.

---
+++ PDPS.js
@@ -0,0 +1,15 @@
+
+
+//POPUP
+
+let popup = document.getElementById('popup2');
+
+function openPopup() {
+    var popup = document.querySelector('.popup2');
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    var popup = document.querySelector('.popup2');
+    popup.classList.remove("open-popup");
+}
